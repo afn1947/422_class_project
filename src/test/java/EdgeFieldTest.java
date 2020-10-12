6e4101bcf6e36bf9d66b7fbadef8983cb83920c8
repl.it user <replituser@example.com>
2020-10-12 00:01:41 +0000

    Added all of the methods to the EdgeFieldTest except for the toString method.

--- src/test/java/EdgeFieldTest.java
+++ src/test/java/EdgeFieldTest.java
@@ -96,57 +96,142 @@ public class EdgeFieldTest {
 		assertEquals("tableBound should not allow a zero value. Attempted to set tableBound to 0.",0,testField.getTableBound());
 	}
 
+	/**
+	* Is the correct file boundary returned?
+	*/
 	@Test
 	public void testGetFileBound() {
 		assertEquals("Test: fileBound should be 0", 0, testField.getFileBound());
 	}
 
+	/**
+	* Can you set the file boundary? 
+	* Can you set the file boundary to zero?
+	* Can you set the file boundary to a negative number?
+	*/
 	@Test
-	public void testSetFileBound() {}
+	public void testSetFileBound() {
+		testField.setFieldBound(1);
+		assertEquals("Test 1: tableBound should be set to 1.",1,testField.getFieldBound());
 
+		testField.setFieldBound(-1);
+		assertEquals("Test 2: tableBound should not allow a negative number. Attempted to set tableBound to -1.",-1,testField.getFieldBound());
+
+		testField.setFieldBound(0);
+		assertEquals("Test 3: tableBound should not allow a zero value. Attempted to set tableBound to 0.",0,testField.getFieldBound());
+	}
+
+	/**
+	* Is the correct value for disallowNull returned?
+	*/
 	@Test
 	public void testGetDisallowNull() {
 		assertEquals("Test: disallowNull should be false", false, testField.getDisallowNull());
 	}
 
+	/**
+	* Can you set the disallowNull property?
+	*/
 	@Test
-	public void testSetDisallowNull() {}
+	public void testSetDisallowNull() {
+		testField.setDisallowNull(true);
+		assertEquals("Test: disallowNull should be set to true",true,testField.getDisallowNull());
+	}
 
+	/**
+	* Is the correct value for isPrimaryKey returned?
+	*/
 	@Test
 	public void testGetIsPrimaryKey() {
 		assertEquals("Test: isPrimaryKey should be false", false, testField.getIsPrimaryKey());
 	}
 
+	/**
+	* Can you set the isPrimaryKey property?
+	*/
 	@Test
-	public void testSetIsPrimaryKey() {}
+	public void testSetIsPrimaryKey() {
+		testField.setIsPrimaryKey(true);
+		assertEquals("Test: isPrimaryKey should be set to true",true,testField.getIsPrimaryKey());
+	}
 
+	/**
+	* Is the correct defaultValue returned?
+	*/
 	@Test
 	public void testGetDefaultValue() {
 		assertEquals("Test: defaultValue should be an empty String", "", testField.getDefaultValue());
 	}
 
+	/**
+	* Can you set the default value of the field to a String?
+	* Can you set the default value of the field to an empty String?
+	*/
 	@Test
-	public void testSetDefaultValue() {}
+	public void testSetDefaultValue() {
+		testField.setDefaultValue("New Field Value");
+		assertEquals("Test 1: defaultValue should now be \"New Field Value\" ","New Field Value",testField.getDefaultValue());
 
-	@Test
-	public void testGetVarCharValue() {
-		assertEquals("Test: varCharValue should be VARCHAR_DEFAULT_LENGTH", VARCHAR_DEFAULT_LENGTH, testField.getVarcharValue());
+		testField.setDefaultValue("");
+		assertEquals("Test 2: defaultValue should now be \"\" ","",testField.getDefaultValue());
 	}
 
+	/**
+	* Is the correct varcharValue returned?
+	*/
 	@Test
-	public void testSetVarCharValue() {}
+	public void testGetVarCharValue() {
+		assertEquals("Test: varCharValue should be 1", 1, testField.getVarcharValue());
+	}
 
+	/**
+	* Can you set the varCharValue? 
+	* Can you set the varCharValue to zero?
+	* Can you set the varCharValue to a negative number?
+	*/
+	@Test
+	public void testSetVarCharValue() {
+		testField.setVarcharValue(1);
+		assertEquals("Test 1: varcharValue should be set to 1.",1,testField.getVarcharValue());
+
+		testField.setVarcharValue(-1);
+		assertEquals("Test 2: varcharValue should not allow a negative number. Attempted to set tableBound to -1.",-1,testField.getVarcharValue());
+
+		testField.setVarcharValue(0);
+		assertEquals("Test 3: varcharValue should not allow a zero value. Attempted to set tableBound to 0.",0,testField.getVarcharValue());
+	}
+
+	/**
+	* Is the correct dataType returned?
+	*/
 	@Test
 	public void testGetDataType() {
 		assertEquals("Test: dataType should be 0", 0, testField.getDataType());
 	}
 
+	/**
+	* Can you set the dataType? 
+	* Can you set the dataType to a negative number?
+	* Can you set the dataType to a number greater than the length of the strDataType array? (3)
+	*/
 	@Test
 	public void testSetDataType() {
-		
+		testField.setDataType(1);
+		assertEquals("Test 1: dataType should be set to 1.",1,testField.getDataType());
+
+		testField.setDataType(-1);
+		assertEquals("Test 2: dataType should not allow a negative number. Attempted to set dataType to -1.",-1,testField.getDataType());
+
+		testField.setDataType(1000);
+		assertEquals("Test 3: dataType should not allow a value > 3. Attempted to set dataType to 1000.",1000,testField.getDataType());
 	}
 
+	/**
+	* Does the toString method return the expected output?
+	*/
 	@Test
-	public void testToString() {}
+	public void testToString() {
+		
+	}
 
 }
